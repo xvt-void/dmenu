12640aec1965c6cd8ded9be1b4b8e832a2dc6d2f
xvt-void <xvt-void@users.noreply.github.com>
2023-09-29 18:36:41 +0200

    x

--- config.def.h
+++ config.def.h
@@ -4,17 +4,19 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Iosevka Nerd Font:size=12",
+	"Font Awesome:size=12",
+	"JoyPixels:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#017ff2", "#101010" },
+	[SchemeSel] = { "#101010", "#017ff2" },
+	[SchemeOut] = { "#000000", "#ff0000" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 9;
 
 /*
  * Characters not considered part of a word while deleting words
